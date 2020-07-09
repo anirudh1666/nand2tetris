012878a105a9a21c43245fd5df5a7d5730c98ee4
Anirudh Lakra <55798132+anirudh1666@users.noreply.github.com>
2020-07-09 12:51:37 +0100

    Finished programming in assembly

--- nand2tetris/nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+	@count          // SCREEN[count] tells us which part to black out or clear.
+	M=0
+	@LOOP 
+	0;JMP 
+
+(LOOP)
+	@KBD            // Listen for keyboard input. If KBD != 0 blackout.
+	D=M 
+	@BLACKOUT
+	D;JNE 
+	
+	@count          // Checks if there are any pixels that we need to black out. 
+	D=M             // Does this by checking if counter < 0. 
+	@LOOP 
+	D;JLT
+	
+	@count          // This part clears the screen. Goes to SCREEN[count]
+	D=M             // and sets that value to 0. Then it decrements count.
+	@SCREEN
+	A=D+A
+	M=0
+	@count 
+	M=M-1 
+	@LOOP 
+	0;JMP 
+	
+(BLACKOUT)
+	@count          // This part blacks out. First it checks if count is out of bounds
+	D=M             // there are 8192 consecutive registers for screen. So if count >
+	@store          // 8192 then go to loop. Else you go to SCREEN[count] and set it 
+	M=D             // to -1 which turns it black. Then decrement count.
+	@8192
+	D=A 
+	@store 
+	D=M-D
+	@LOOP 
+	D;JEQ
+	
+	@count 
+	D=M 
+	@SCREEN
+	A=D+A 
+	M=-1
+	@count 
+	M=M+1
+	@LOOP
+	0;JMP
+	
+	
+	
+	
--- nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// This algorithm works by adding R0 to R2 (starting value = 0) R1 times.
+
+	@2
+	M=0              // Load zero into R2 
+	@LOOP
+	0;JMP
+(LOOP)
+	@1               // Check if R2 == 0. If so then go to end.
+	D=M  
+	@END
+	D;JEQ  
+	@0               // Otherwise, R2 = R2 + R1 
+	D=M
+	@2 
+	M=D+M
+	@1               // Decrement R2. 
+	M=M-1
+	@LOOP 
+	0;JMP
+(END)
+	@END
+	0;JMP
+	
+
